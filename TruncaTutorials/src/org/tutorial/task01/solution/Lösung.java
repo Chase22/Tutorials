cbaa36914105e70241c4ce5632833b7ba3ca537e
Chase <dies200@rocketmail.com>
2017-10-08 20:54:27 +0200

    Erste Aufgabe

---
+++ TruncaTutorials/src/org/tutorial/task01/solution/Lösung.java
@@ -0,0 +1,35 @@
+package org.tutorial.task01.solution;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Lösung {
+
+	public static void main(String[] args) {
+		String name = ""; // Initialisieren der String Variable. Nicht unbedingt nötig aber empfohlen
+		
+		//Eine furchbare Schachtelung die einen BufferedReader mit einem InputStreamReader initialisiert der den System.in Input Stream ausliest
+		//(Siehe Aufgabenbeschreibung für Details)
+		BufferedReader inputReader = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		try {
+			//Begüßung und Frage nach dem Namen
+			System.out.println("Guten Tag. Wie heißt du?");
+			
+			//Lesens einer Zeile aus der Konsole
+			name = inputReader.readLine();
+			
+			//Der einfache Weg einer Ausgabe
+			//System.out.println("Hallo " + name);
+			
+			//Der etwas schönere Weg einer Ausgabe. %s steht hierbei für einen String
+			//Weitere Infos: http://openbook.rheinwerk-verlag.de/javainsel9/javainsel_04_010.htm#t2t31 (Strg+Klick zum öffnen)
+			System.out.println(String.format("Hallo %s", name));
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
